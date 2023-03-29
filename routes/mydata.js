ba8410d399aa87587e48582a1505f7bcce1b994d
keerthisannapareddy <123145377+keerthisannapareddy@users.noreply.github.com>
2023-03-29 18:32:43 -0500

    created User endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-let sum = 0;
-let more = 0;
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  more = more+1;
-  sum = sum +more;
-  res.send(`Sum is : ${sum}`)
+  res.render('mydata', { title: 'Keerthy Sannapureddy' });
 });
 
 module.exports = router;
